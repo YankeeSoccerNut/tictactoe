892de20e99df8ac0a71dd767592618a35bfbf797
Scott Anderson <yankeesoccernut@gmail.com>
2017-10-04 07:07:18 -0400

    Player constructor, some jQuery, and computerTurn

--- scripts.js
+++ scripts.js
@@ -1,42 +1,51 @@
 // TODO: Turns feature....1 player (against computer), 2 player1Squares
-// TODO: Reset button
 // TODO: Give computer intelligence to try to win!!
 
 //GLOBALS
-var testClickCount = 0;
+numPlayers = 1;
 var boxSize = 0;
 var gameWinnerCombo = [];
 haveWinner = false;
 var whosTurn = 1;
 var squares = [];
-player1Squares = [];
-player2Squares = [];
+
+// player1Squares = [];
+// player2Squares = [];
 var OPEN_SQUARE = '-';
 var DEFAULT_SQ_CLASS = "square";
 var winningCombos =[];
+// var player1PotentialWinCombos[];
+// var player2PotentialWinCombos[];
 
-// var winningCombos = [['00', '10', '20']
-// ,['01', '11', '21']
-// ,['02', '12', '22']
-// ,['00', '01', '02']
-// ,['10', '11', '12']
-// ,['20', '21', '22']
-// ,['00', '11', '22']
-// ,['02', '11', '20']];
 
-// Setup board (in HTML)
+function Player() {     // Constructor for players
+    this.minMovesToWin = 0;
+    this.numWinningCombosRemaing = 0;
+    this.potentialWinCombos = [];
+    this.squares = [];
+}
 
-// User clicks a button then put that player's mark on the board
+var player1 = new Player;
+var player2 = new Player;
+
 
 // Need to track turns!
 // Check game status each turn...
 // Declare winner when it happens
 // Or draw at end and no winner.
+// Setup board (in HTML)
+
+$(document).ready(run);
+
+function run(){
+  if (squares.length == 0){  //  first time, not reset
+    setupBoard(true);
+  }
+}
 
 function setupBoard(initial){
   var gameRowsHTML = "";
   var winningCombo = [];
-
   boxSize = parseInt(prompt("Please enter number of rows"));
 
 
@@ -44,7 +53,7 @@ function setupBoard(initial){
     gameRowsHTML += '<div class="board-row">';  // opening for each row
     winningCombo = [];
     for (let columns = 0; columns < boxSize; columns++){
-      gameRowsHTML += `<button id="${rows}${columns}" class="${DEFAULT_SQ_CLASS}">${OPEN_SQUARE}</button>`;
+      gameRowsHTML += `<button id="${rows}${columns}" class="btn-lg ${DEFAULT_SQ_CLASS}">${OPEN_SQUARE}</button>`;
       winningCombo.push(`${rows}${columns}`);
     } // end columns
     gameRowsHTML += '</div>'  // closing div for the gameRow
@@ -65,13 +74,12 @@ function setupBoard(initial){
     winningCombos.push(winningCombo);
   }  // end columns
 
-  console.log(winningCombos);
-  //now we need the diags.....
+  //now we need the diags.....forward and backward
   winningCombo = [];
   for (let i = 0; i < boxSize; i++){  //forward
     winningCombo.push(`${i}${i}`);
   }
-  winningCombos.push(winningCombo);
+  winningCombos.push(winningCombo);  // fwd diag
 
   winningCombo = [];
     rows = 0;
@@ -79,31 +87,35 @@ function setupBoard(initial){
       winningCombo.push(`${rows}${i}`);
       rows++;
     }
-  winningCombos.push(winningCombo);
+  winningCombos.push(winningCombo);  // bkwd diag
 
-// array of 9 objects, each object is a representation of the button object.
+// winningCombos is fully loaded....copy to each players potetnial Wins
+  player1.potentialWinCombos = winningCombos.slice(0,winningCombos.length);
+  player2.potentialWinCombos = winningCombos.slice(0,winningCombos.length);
+
+// array of boxSize*boxSize objects, each object is a representation of the button object.
   squares = document.getElementsByClassName(DEFAULT_SQ_CLASS);
 
   for (let i = 0; i < squares.length; i++){
     // we can dynamically add an event listener to each...keeps it out of the HTML
     // using an anonymous function here...meaning it can only be called when 'click' occurs
-    // squares[i].innerHTML = OPEN_SQUARE;  // wipe board, set to dash
 
     squares[i].addEventListener('click', function(event){
-    console.log("addEventListener on squares");
-    console.log(this);
     if (!haveWinner){
       markSquare(this);
     }
     });
   }
-  // add Listener on Reset button....
+  // add Listener on Reset
   if (initial == true){
-    document.getElementById('reset').addEventListener('click', function(event){
+    $('#reset').click(resetGame);
+    $('#one-player').click(function () {
+      numPlayers = 1;
       resetGame(this);
     });
-    document.getElementById('number-spinner-btn').addEventListener('click', function(event){
-      handleSpinner(this);
+    $('#two-player').click(function () {
+      numPlayers = 2;
+      resetGame(this);
     });
   }
 } //end setupBoard
@@ -122,6 +134,8 @@ function handleSpinner(spinner){
   }
   spinner.closest('.number-spinner').find('input').val(newVal);
 }
+
+
 function resetGame(resetClicked){
 
   // Clear out the buttons in the DOM
@@ -139,8 +153,15 @@ function resetGame(resetClicked){
   gameWinnerCombo = [];
   haveWinner = false;
   whosTurn = 1;
-  player1Squares = [];
-  player2Squares = [];
+  player1.minMovesToWin = 0;
+  player1.numWinningCombosRemaing = 0;
+  player1.potentialWinCombos = [];
+  player1.squares = [];
+  player2.minMovesToWin = 0;
+  player2.numWinningCombosRemaing = 0;
+  player2.potentialWinCombos = [];
+  player2.squares = [];
+
   squares = [];
 
   // Now set up the board...
@@ -148,9 +169,10 @@ function resetGame(resetClicked){
 
 }  // end resetGame
 
-// 2 things happen onClick....DOM is changed, vars are changedwin
+// This is the listener for click on each Square
+// User clicks a button then put that player's mark on the board
+
 function markSquare(squareClicked){
-  testClickCount += 1;
   var tempText = squareClicked.innerHTML;
   if(squareClicked.innerHTML != OPEN_SQUARE){
     document.getElementById('message').innerHTML = "Sorry, that square is occupied";
@@ -158,21 +180,33 @@ function markSquare(squareClicked){
   else if (whosTurn == 1){
     document.getElementById('message').innerHTML = "";
     squareClicked.innerHTML = "X";
-    player1Squares.push(squareClicked.id);
-    haveWinner = checkWin(player1Squares, "Player 1 Wins!");
-    whosTurn = 2;
+    player1.squares.push(squareClicked.id);
+    haveWinner = checkWin(player1.squares, "Player 1 Wins!");
+    if ((!haveWinner) && (numPlayers == 1)){
+      // Eliminate potential winning combos from Player 2 that rely on this square  (Computer is Player 2)
+      reducePotentialWinCombos(player2, squareClicked.id);
+      computerTurn();  // no need to pass anything...computer knows all ;-)
+      haveWinner = checkWin(player2.squares, "Computer Wins!");
+      whosTurn = 1;
+    }
+    else{
+      whosTurn = 2;
+    }
   }
-  else{
+  else{    // Human Player 2's turn
     document.getElementById('message').innerHTML = "";
     squareClicked.innerHTML = "O";
-    player2Squares.push(squareClicked.id);
-    haveWinner = checkWin(player2Squares, "Player 2 Wins");
+    player2.squares.push(squareClicked.id);
+    haveWinner = checkWin(player2.squares, "Player 2 Wins");
     whosTurn = 1;
   }
+
   if (haveWinner){
     markWinner();
   }
 }
+
+//Determine if there is a winner....uses the winningCombos array that was built during setupBoard
 
 function checkWin(currentPlayer, winMsg){
   //Outer loop checks combinations...
@@ -196,12 +230,39 @@ function checkWin(currentPlayer, winMsg){
 }
 
 function markWinner() {
+
   for (let i = 0; i < gameWinnerCombo.length; i++){
     var theTargetSquare = gameWinnerCombo[i];
-    document.getElementById([theTargetSquare]).className += ' winning-square';
+    $(`#${theTargetSquare}`).addClass('winning-square');
+    // document.getElementById([theTargetSquare]).className += ' winning-square';
   }
 }
 
-if (squares.length == 0){  //  first time, not reset
-  setupBoard(true);
+function reducePotentialWinCombos(player, squareId){
+  console.log("reducePotentialWinCombos");
+  console.log(squareId);
+}
+
+function computerTurn(){
+  var computerPick;
+
+  console.log("computerTurn...");
+  // simple logic for now...take any open square....
+  //$('div:contains("test"):last')
+  computerPick = $('button:contains("-"):first');  // returns an array!!
+  console.log(computerPick[0].id);
+
+  // Save the players move for checking against win combos...
+  player2.squares.push(computerPick[0].id);
+
+  // Update the DOM with the Computer's Pick
+  // $('#01').html('O');  This Works Explicitly....
+
+  // tempText = '#' + computerPick[0].id; // build valid target for jQuery
+
+  $(`#${computerPick[0].id}`).html('O');
+
+  // Native js works!
+  // document.getElementById(computerPick[0].id).innerHTML = 'O';
+
 }
